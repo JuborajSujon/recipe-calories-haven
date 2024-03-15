037473bfa96bfb9e03737b1f9bc5d9920e99b346
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-16 02:30:43 +0600

    banner button and typography adjust

--- src/components/Banner Container/BannerContainer.jsx
+++ src/components/Banner Container/BannerContainer.jsx
@@ -2,7 +2,7 @@ export default function BannerContainer() {
   return (
     <section className="rounded-3xl">
       <div
-        className="hero h-[75vh] lg:h-[60vh] bg-cover bg-no-repeat bg-center rounded-3xl"
+        className="hero h-[70vh] lg:h-[60vh] bg-cover bg-no-repeat bg-center rounded-3xl"
         style={{
           backgroundImage:
             "linear-gradient(90deg,rgba(21,11,43,0.7),rgba(21,11,43,0.0)),url(https://i.ibb.co/X7ftJpf/banner.jpg)",
@@ -10,7 +10,7 @@ export default function BannerContainer() {
         <div className="hero-overlay opacity-0 rounded-3xl"></div>
         <div className="hero-content text-center text-white">
           <div className="max-w-3xl">
-            <h1 className="mb-5 text-2xl lg:text-5xl font-bold">
+            <h1 className="mb-5 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
               Welcome to Flavor Haven: Indulge in Exquisite Flavors
             </h1>
             <p className="mb-5">
@@ -19,8 +19,13 @@ export default function BannerContainer() {
               classics to innovative creations, our menu is crafted to tantalize
               your taste buds and leave you craving for more.
             </p>
-            <div>
-              <button className="btn btn-primary">Get Started</button>
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-3 md:gap-5">
+              <button className="py-2 px-6 md:py-3.5 md:px-9 lg:py-4 lg:px-12 font-bold text-sm md:text-base lg:text-xl border border-secondary bg-secondary text-primary rounded-full">
+                Explor Now
+              </button>
+              <button className="py-2 px-4 md:py-3.5 md:px-6 lg:py-4 lg:px-8 font-bold text-sm md:text-base lg:text-xl border border-white bg-transparent text-white rounded-full">
+                Our Feedback
+              </button>
             </div>
           </div>
         </div>
